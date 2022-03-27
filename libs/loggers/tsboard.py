edd07204b0a8b9d77473ee7f66ee9cf1704a1b3e
trungnq0202 <s3742774@rmit.edu.vn>
2022-03-27 15:08:26 +0700

    add logger

---
+++ libs/loggers/tsboard.py
@@ -0,0 +1,12 @@
+from torch.utils.tensorboard import SummaryWriter
+
+class TensorboardLogger():
+    def __init__(self, path) -> None:
+        assert path != None, "path is None"
+        self.writer = SummaryWriter(log_dir=path)
+
+    def update_loss(self, phase, value, step):
+        self.writer.add_scalar(f'{phase}/loss', value, step)
+
+    def update_metric(self, phase, metric, value, step):
+        self.writer.add_scalar(f'{phase}/{metric}', value, step)
